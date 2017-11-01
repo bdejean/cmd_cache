2c46a9767ea2fa2ce3ca7a2a04e78a346eba808a
Cyprien Le Pannérer <cyplp@free.fr>
2017-11-01 10:16:47 +0100

    add hash function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+extern crate crypto;
+
 use std::env;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
 
 fn concat_args(mut args : Vec<String>) -> String {
     args.remove(0);
@@ -6,8 +11,19 @@ fn concat_args(mut args : Vec<String>) -> String {
     return joined;
 }
 
+fn hash(s :String) -> String {
+    let mut h = Md5::new();
+    h.input_str(&s);
+    return h.result_str();
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("{}", concat_args(args));
+    let joined = concat_args(args);
+    println!("{}", joined);
+
+    let md5 = hash(joined);
+
+    println!("{}", md5);
 }
