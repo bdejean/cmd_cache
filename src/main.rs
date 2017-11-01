bded6b8a6d78fb146319381673e9ae6d3102457e
Cyprien Le Pannérer <cyplp@free.fr>
2017-11-01 09:59:05 +0100

    joined args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+fn concat_args(mut args : Vec<String>) -> String {
+    args.remove(0);
+    let joined = args.join("@@join@@");
+    return joined;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    println!("{}", concat_args(args));
 }
